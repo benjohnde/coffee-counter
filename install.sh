5ed72ce7528c46dd7ca24c9e8ccf511913fce60f
Ben John <github@benjohn.de>
2018-02-15 08:26:15 +0100

    Upgrade condition in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 mkdir -p ~/.coffee
 cd ~/.coffee
 curl -sSfO https://raw.githubusercontent.com/benjohnde/coffee-counter/master/coffee.sh
 chmod +x coffee.sh
-echo "alias coffee='sh ~/.coffee/coffee.sh' # handy shortcut for coffee-counter" >> ~/.bash_profile
-source ~/.bash_profile
-coffee init
+
+if ! cat ~/.bash_profile | grep -q "handy shortcut for coffee-counter"; then
+  echo "alias coffee='sh ~/.coffee/coffee.sh' # handy shortcut for coffee-counter" >> ~/.bash_profile
+  source ~/.bash_profile
+  coffee init
+fi
